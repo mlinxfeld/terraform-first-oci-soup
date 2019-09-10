7a5d5cb163ca5eb513c20d70b9b97a1bec1bb894
lukasz_feldman <lukasz.feldman@oracle.com>
2019-09-10 16:43:25 -0700

    After fixing compartment.tf and variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key" {}
 variable "compartment_ocid" {}
 variable "region" {}
 variable "private_key_oci" {}
