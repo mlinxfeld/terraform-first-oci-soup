e8900ea692ad4fd993ba75a526f858227eecbe4f
lukasz_feldman <lukasz.feldman@oracle.com>
2019-09-11 14:35:40 -0700

    TF 0.12 test

--- security_list.tf
+++ security_list.tf
@@ -1,6 +1,10 @@
+locals {
+  service_ports = [{min=80,max=80},{min=443,max=443},{min=22,max=22}]
+}
+
 resource "oci_core_security_list" "FoggyKitchenSecurityList" {
     compartment_id = "${oci_identity_compartment.FoggyKitchenCompartment.id}"
-    display_name = "FoggyKitchenHTTPSecurityList"
+    display_name = "FoggyKitchenSecurityList"
     vcn_id = "${oci_core_virtual_network.FoggyKitchenVCN.id}"
     
     egress_security_rules {
@@ -8,32 +12,43 @@ resource "oci_core_security_list" "FoggyKitchenSecurityList" {
         destination = "0.0.0.0/0"
     }
     
-    ingress_security_rules {
-        protocol = "6"
+    dynamic "ingress_security_rules" {
+    for_each = local.service_ports
+    content {
+      protocol = "6"
         source = "0.0.0.0/0"
         tcp_options {
-            max = "80"
-            min = "80"
+            max = ingress_security_rules.value.min
+            min = ingress_security_rules.value.max
         }
     }
 
-    ingress_security_rules {
-        protocol = "6"
-        source = "0.0.0.0/0"
-        tcp_options {
-            max = "443"
-            min = "443" 
-        }
-    }
+#    ingress_security_rules {
+#        protocol = "6"
+#        source = "0.0.0.0/0"
+#        tcp_options {
+#            max = "80"
+#            min = "80"
+#        }
+#    }
 
-    ingress_security_rules {
-        protocol = "6"
-        source = "0.0.0.0/0"
-        tcp_options {
-            max = "22"
-            min = "22"
-        }
-    }
+#    ingress_security_rules {
+#        protocol = "6"
+#        source = "0.0.0.0/0"
+#        tcp_options {
+#            max = "443"
+#            min = "443" 
+#        }
+#    }
+
+#    ingress_security_rules {
+#        protocol = "6"
+#        source = "0.0.0.0/0"
+#        tcp_options {
+#            max = "22"
+#            min = "22"
+#        }
+#    }
 
 	ingress_security_rules {
 	    protocol = "6"
